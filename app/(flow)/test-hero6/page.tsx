2b7b26407e61017d970d4ad1186c8c3b21b7406e
hrid1 <hridoycse1@gmail.com>
2026-02-05 14:43:12 +0600

    update different hero

---
+++ app/(flow)/test-hero6/page.tsx
@@ -0,0 +1,20 @@
+import ContactSection from "@/components/pages/homepage/ContactSection";
+import Hero from "@/components/pages/homepage/HeroSection";
+import TrustedCompany from "@/components/pages/homepage/TrustedCompany";
+import FeatureSection from "@/components/pages/homepage/FeatureSection";
+import HowItWorks from "@/components/pages/homepage/HowItWorks";
+import Testimonials from "@/components/pages/homepage/Testimonials";
+import AboutUs from "@/components/pages/homepage/AboutUs";
+import { TestHeroBgImage } from "../test-hero2/page";
+
+export default function TestHero() {
+    return (
+        <div>
+
+            <TestHeroBgImage bgImage="hero-banner7.jpg" />
+            <TrustedCompany />
+            <FeatureSection />
+
+        </div>
+    );
+}
